86ce34ca2d0198980de13bf69f2e532319825786
Nishanth <nishanthkc009@gmail.com>
2018-12-30 20:24:06 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,13 +4,13 @@ git clone https://github.com/udacity/deep-reinforcement-learning.git
 cd deep-reinforcement-learning/python
 pip install .
 python -m ipykernel install --user --name drlnd --display-name "drlnd"
-git clone https://github.com/Nishanth009/Udacity-Reinforcement-learning-Project---Continous-Control.git
-cd Udacity-Reinforcement-learning-Project---Continous-Control
-wget "https://s3-us-west-1.amazonaws.com/udacity-drlnd/P2/Reacher/Reacher_Linux.zip"
-unzip Reacher_Linux.zip
-wget "https://s3-us-west-1.amazonaws.com/udacity-drlnd/P2/Reacher/Reacher.app.zip"
-unzip Reacher.app.zip
-wget "https://s3-us-west-1.amazonaws.com/udacity-drlnd/P2/Reacher/Reacher_Windows_x86.zip"
-unzip Reacher_Windows_x86.zip
-wget "https://s3-us-west-1.amazonaws.com/udacity-drlnd/P2/Reacher/Reacher_Windows_x86_64.zip"
-unzip Reacher_Windows_x86_64.zip
+git clone https://github.com/Nishanth009/Udacity-Nanodegree-Collaboration-Competition-RL-Project.git
+cd Udacity-Nanodegree-Collaboration-Competition-RL-Project
+wget "https://s3-us-west-1.amazonaws.com/udacity-drlnd/P3/Tennis/Tennis_Linux.zip"
+unzip Tennis_Linux.zip
+wget "https://s3-us-west-1.amazonaws.com/udacity-drlnd/P3/Tennis/Tennis.app.zip"
+unzip Tennis.app.zip
+wget "https://s3-us-west-1.amazonaws.com/udacity-drlnd/P3/Tennis/Tennis_Windows_x86.zip"
+unzip Tennis_Windows_x86.zip
+wget "https://s3-us-west-1.amazonaws.com/udacity-drlnd/P3/Tennis/Tennis_Windows_x86_64.zip"
+unzip Tennis_Windows_x86_64.zip
